e2e2235324736ac85b4133c3c3a29d364fa333c4
Siva <siva@Admins-MacBook-Pro.local>
2025-07-01 10:44:25 +0530

    added firebase crashlytics

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+plugins {
+    // Make sure that you have the AGP plugin 8.1+ dependency
+    id("com.android.application") version "8.7.3" apply false
+    // ...
+
+    // Make sure that you have the Google services Gradle plugin 4.4.1+ dependency
+    id("com.google.gms.google-services") version "4.4.1" apply false // Or a newer version
+
+    // Add the dependency for the Crashlytics Gradle plugin
+    id("com.google.firebase.crashlytics") version "3.0.4" apply false // Or a newer version
+}
+
 allprojects {
     repositories {
         google()
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:find_a_surveyor/service/database_service.dart';
 import 'package:find_a_surveyor/service/firestore_service.dart';
 import 'package:find_a_surveyor/theme/app_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,6 +14,14 @@ void main() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(
       MultiProvider(
         providers: [
